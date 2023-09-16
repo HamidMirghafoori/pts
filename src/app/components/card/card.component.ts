e0d9e8f58a0d194478f81595d6ba13fb61e18dd7
Hamid Mirghafoori <mirghafooris@ryanair.com>
2023-09-16 12:14:36 +0100

    card design

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+export interface CardInfo {
+  bgImg: string;
+  category: string;
+  destination: string;
+  title: string;
+  rate: number;
+  votes: number;
+  bookedCount: number;
+  tags: string[];
+  price: number;
+  currency: string;
+}
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  preserveWhitespaces: true 
 })
 export class CardComponent {
-
+  @Input() data: CardInfo = {
+    bgImg: '../../../assets/1.png',
+    category: 'Rail Passes',
+    destination: 'Tokyo',
+    title: 'JR Pass for Whole Japan',
+    rate: 4.5,
+    votes: 20654,
+    bookedCount: 500452,
+    tags: ['Bestseller'],
+    price: 201.69,
+    currency: 'US$',
+  };
 }
