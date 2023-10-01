e9943b427791d8102cf3f3dc7c8d9d20cb513cfd
Hamid Mirghafoori <mirghafooris@ryanair.com>
2023-10-01 16:06:34 +0100

    final

--- src/app/components/shop-products/shop-products.component.ts
+++ src/app/components/shop-products/shop-products.component.ts
@@ -1,21 +1,5 @@
-import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import {
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild,
-  inject,
-} from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
-import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
-import { MatChipInputEvent } from '@angular/material/chips';
-import { Observable, map, startWith } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProductType, ShopService } from 'src/app/services/shop.service';
 
 @Component({
@@ -27,15 +11,7 @@ export class ShopProductsComponent implements OnInit {
   products: ProductType[] = [];
   panelOpen = false;
   productForm!: FormGroup;
-  announcer = inject(LiveAnnouncer);
-  separatorKeysCodes: number[] = [ENTER, COMMA];
-  tags: string[] = [];
-  tagCtrl = new FormControl('');
-  filteredTags!: Observable<string[]>;
   allTags: string[] = ['Bestseller', 'Easy Refund'];
-  offers: string[] = [];
-  offerCtrl = new FormControl('');
-  filteredOffers!: Observable<string[]>;
   allOffers: string[] = [
     'Exclusive Combo',
     'Best Price Guarantee',
@@ -44,23 +20,7 @@ export class ShopProductsComponent implements OnInit {
     'PTS Exclusive',
   ];
 
-  @ViewChild('tagInput') tagInput!: ElementRef<HTMLInputElement>;
-  @ViewChild('offerInput') offerInput!: ElementRef<HTMLInputElement>;
-
-  constructor(private shopService: ShopService, private fb: FormBuilder) {
-    this.filteredTags = this.tagCtrl.valueChanges.pipe(
-      startWith(null),
-      map((tag: string | null) =>
-        tag ? this._filterTags(tag) : this.allTags.slice()
-      )
-    );
-    this.filteredOffers = this.offerCtrl.valueChanges.pipe(
-      startWith(null),
-      map((offer: string | null) =>
-        offer ? this._filterOffers(offer) : this.allOffers.slice()
-      )
-    );
-  }
+  constructor(private shopService: ShopService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.productForm = this.fb.group({
@@ -68,83 +28,16 @@ export class ShopProductsComponent implements OnInit {
       category: ['', Validators.required],
       destination: ['', Validators.required],
       price: ['', Validators.required],
-      tags: this.tagCtrl,
-      offers: [['']],
+      tags: [''],
+      offers: [''],
       image: [-1],
     });
-  
+
     this.shopService.getAllProducts().subscribe((products) => {
       this.products = products;
     });
   }
 
-  addTag(event: MatChipInputEvent): void {
-    const value = (event.value || '').trim();
-
-    if (value) {
-      this.tags.push(value);
-    }
-    event.chipInput!.clear();
-
-    this.tagCtrl.setValue(null);
-  }
-
-  removeTag(tag: string): void {
-    const index = this.tags.indexOf(tag);
-
-    if (index >= 0) {
-      this.tags.splice(index, 1);
-
-      this.announcer.announce(`Removed ${tag}`);
-    }
-  }
-
-  selectedTag(event: MatAutocompleteSelectedEvent): void {
-    this.tags.push(event.option.viewValue);
-    this.tagInput.nativeElement.value = '';
-    this.tagCtrl.setValue(null);
-  }
-  addOffer(event: MatChipInputEvent): void {
-    const value = (event.value || '').trim();
-
-    if (value) {
-      this.offers.push(value);
-    }
-    event.chipInput!.clear();
-
-    this.offerCtrl.setValue(null);
-  }
-
-  removeOffer(offer: string): void {
-    const index = this.offers.indexOf(offer);
-
-    if (index >= 0) {
-      this.offers.splice(index, 1);
-
-      this.announcer.announce(`Removed ${offer}`);
-    }
-  }
-
-  selectedOffer(event: MatAutocompleteSelectedEvent): void {
-    this.offers.push(event.option.viewValue);
-    this.offerInput.nativeElement.value = '';
-    this.offerCtrl.setValue(null);
-  }
- 
-  private _filterTags(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    
-    return this.allTags.filter((tag) =>
-    tag.toLowerCase().includes(filterValue)
-    );
-  }
-  private _filterOffers(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    
-    return this.allOffers.filter((offer) =>
-    offer.toLowerCase().includes(filterValue)
-    );
-  }
 
   // export interface Product {
   //   title: string;
@@ -161,7 +54,7 @@ export class ShopProductsComponent implements OnInit {
   // }
   onSubmit() {
     const form = this.productForm.value;
-    console.log(form, this.tagCtrl);
+    console.log(form);
     
   }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -8,6 +8,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -23,6 +24,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatSelectModule,
     MatSlideToggleModule,
     MatSnackBarModule,
     MatToolbarModule,
@@ -37,6 +39,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatSelectModule,
     MatSlideToggleModule,
     MatSnackBarModule,
     MatToolbarModule,
