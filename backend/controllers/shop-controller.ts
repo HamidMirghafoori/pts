285c5a928391e857a3d506e6bb9fe0819bf0b0f5
Hamid Mirghafoori <mirghafooris@ryanair.com>
2023-11-11 16:25:55 +0000

    BE: getShopReport function

--- backend/controllers/shop-controller.ts
+++ backend/controllers/shop-controller.ts
@@ -11,8 +11,8 @@ export const purchaseItem = async (
   res: Response,
   next: NextFunction
 ) => {
-  console.log('purchaseItem....');
-  
+  console.log("purchaseItem....");
+
   try {
     const { userId } = req.body;
     const { productId } = req.body;
@@ -110,7 +110,7 @@ export const getPurchases = async (
       _id: { $in: productIds },
     }).lean()) as ProductType[];
 
-        // TODO: add rate and votes need to be added to the response based on purchases info
+    // TODO: add rate and votes need to be added to the response based on purchases info
     const products = productsRes.map((product: any) => {
       return {
         ...product,
@@ -163,8 +163,8 @@ export const ratePurchase = async (
     const { productId } = req.body;
     const { purchaseId } = req.body;
     const { rate } = req.body;
-    console.log('rating....', userId, productId, purchaseId, rate);
-    
+    console.log("rating....", userId, productId, purchaseId, rate);
+
     if (!mongoose.Types.ObjectId.isValid(userId)) {
       return res.status(400).json({ message: "Invalid userId" });
     }
@@ -225,14 +225,14 @@ export const addRevenue = async (
   next: NextFunction
 ) => {
   try {
-    const { quantity } = req.body; 
-    const { customerEmail } = req.body;  
-    const { productId } = req.body; 
-    const { customerId } = req.body; 
-    const { productName } = req.body;  
-    const { shopId } = req.body; 
-    const { shopEmail } = req.body; 
-    const { price } = req.body; 
+    const { quantity } = req.body;
+    const { customerEmail } = req.body;
+    const { productId } = req.body;
+    const { customerId } = req.body;
+    const { productName } = req.body;
+    const { shopId } = req.body;
+    const { shopEmail } = req.body;
+    const { price } = req.body;
 
     if (!mongoose.Types.ObjectId.isValid(shopId)) {
       return res.status(400).json({ message: "shopId userId" });
@@ -305,4 +305,111 @@ export const addRevenue = async (
       error,
     });
   }
+};
+
+export const getShopReport = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const report = {
+    products: [
+      {
+        productId: "654806d034ab6fa73deab36a",
+        title: "Saiful Muluk sh3",
+        count: 12,
+      },
+      { productId: "6548076134ab6fa73deab379", title: "Tohoka sh3", count: 16 },
+      { productId: "6548070134ab6fa73deab36f", title: "Pattaya sh3", count: 8 },
+    ],
+    sales: [
+      { date: "Jan", gross: 2750 },
+      { date: "Feb", gross: 2150 },
+      { date: "Mar", gross: 1200 },
+      { date: "May", gross: 3700 },
+      { date: "Apr", gross: 2200 },
+      { date: "Jun", gross: 2450 },
+      { date: "Jul", gross: 2000 },
+    ],
+    productSales: [
+      {
+        date: "Jan",
+        product: [
+          {
+            id: "654806d034ab6fa73deab36a",
+            title: "Saiful Muluk sh3",
+            count: 2,
+          },
+        ],
+      },
+      {
+        date: "Feb",
+        product: [
+          {
+            id: "654806d034ab6fa73deab36a",
+            title: "Saiful Muluk sh3",
+            count: 6,
+          },
+          { id: "6548076134ab6fa73deab379", title: "Tohoka sh3", count: 8 },
+        ],
+      },
+      {
+        date: "Mar",
+        product: [
+          {
+            id: "654806d034ab6fa73deab36a",
+            title: "Saiful Muluk sh3",
+            count: 1,
+          },
+          { id: "6548076134ab6fa73deab379", title: "Tohoka sh3", count: 2 },
+          { id: "6548070134ab6fa73deab36f", title: "Pattaya sh3", count: 10 },
+        ],
+      },
+      {
+        date: "May",
+        product: [
+          { id: "6548070134ab6fa73deab36f", title: "Pattaya sh3", count: 8 },
+        ],
+      },
+      {
+        date: "Apr",
+        product: [
+          {
+            id: "654806d034ab6fa73deab36a",
+            title: "Saiful Muluk sh3",
+            count: 2,
+          },
+          { id: "6548070134ab6fa73deab36f", title: "Pattaya sh3", count: 3 },
+        ],
+      },
+      {
+        date: "Jun",
+        product: [
+          {
+            id: "654806d034ab6fa73deab36a",
+            title: "Saiful Muluk sh3",
+            count: 3,
+          },
+          { id: "6548076134ab6fa73deab379", title: "Tohoka sh3", count: 6 },
+        ],
+      },
+      {
+        date: "Jul",
+        product: [
+          {
+            id: "654806d034ab6fa73deab36a",
+            title: "Saiful Muluk sh3",
+            count: 5,
+          },
+          { id: "6548076134ab6fa73deab379", title: "Tohoka sh3", count: 3 },
+          { id: "6548070134ab6fa73deab36f", title: "Pattaya sh3", count: 4 },
+        ],
+      },
+    ],
+  };
+  return res.status(200).json({
+    success: true,
+    message: "Revenue is added",
+    report,
+  });
 };
